6d3a0d4dfafe75689f8badfc448415b6bcd7a8b9
LucaMawyin <lucamawyin@gmail.com>
2026-06-16 17:42:25 -0400

    Starting new yolo model

--- yolo_model.py
+++ yolo_model.py
@@ -3,12 +3,12 @@ from ultralytics import YOLO
 def main():
 
     # Load a pretrained YOLO26n model
-    model = YOLO("yolo26m.pt")
+    model = YOLO("yolo26s.pt")
 
     # Train the model on the config dataset for 100 epochs
     model.train(
         data="config.yaml",  # Path to dataset configuration file
-        epochs=100,  # Number of training epochs
+        epochs=50,  # Number of training epochs
         device=0, # Nvidia 3080ti
         workers=8, # 14 cores 20 threads
         project=r"C:\Users\lucam\Desktop\Code\signlanguage\runs", # Explicitly state dir (saving to wrong dir)
