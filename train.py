95b78a22f59ee0ad51c8fc46c989413640ebc19b
lucamawyin <lucamawyin@gmail.com>
2026-06-15 21:54:03 -0400

    Upped to 30 epochs

--- train.py
+++ train.py
@@ -6,5 +6,5 @@ model = YOLO("yolo26n-pose.pt")
 # Train the model on the config dataset for 100 epochs
 train_results = model.train(
     data="config.yaml",  # Path to dataset configuration file
-    epochs=3,  # Number of training epochs
+    epochs=30,  # Number of training epochs
 )
