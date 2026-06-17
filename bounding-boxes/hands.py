2bba2a0715f70c0f71ce4875815c47bdbf475e44
LucaMawyin <lucamawyin@gmail.com>
2026-06-17 00:41:45 -0400

    updated hands

--- bounding-boxes/hands.py
+++ bounding-boxes/hands.py
@@ -2,10 +2,12 @@ import cv2
 from ultralytics import YOLO
 
 cap = cv2.VideoCapture(0)
-model = YOLO("runs/hand_pose/weights/best.pt").to("cuda")
+model = YOLO("runs/hand_pose/weights/last.pt").to("cuda")
 
 text_str = ""
 last_class = None
+stable_count = 0
+required_frames = 10
 
 if not cap.isOpened():
     print("Error: Could not open webcam.")
@@ -21,28 +23,35 @@ while True:
         break
 
     # YOLO hand detection
-    results = model(frame, conf=0.1, iou=0.5, verbose=False)
+    results = model(frame, conf=0.1, verbose=False)
 
     r = results[0]
 
     # Get best detection (if any exist)
     if r.boxes is not None and len(r.boxes) > 0:
-        cls_id = int(r.boxes.cls[0])
-        conf = float(r.boxes.conf[0])
+        best_idx = r.boxes.conf.argmax()
+
+        cls_id = int(r.boxes.cls[best_idx])
+        conf = float(r.boxes.conf[best_idx])
         class_name = model.names[cls_id]
 
-        if class_name == "space" and conf < 0.5:
-            continue
 
-        # only print if changed
-        if class_name != last_class:
+        if class_name == last_class:
+            stable_count += 1
+        else:
+            stable_count = 0
+            last_class = class_name
+
+        # Only print on stabilized frames
+        if stable_count == required_frames:
+
             if class_name == "del":
                 text_str = text_str[:-1]
             elif class_name == "space":
                 text_str += "_"
             else:
                 text_str += class_name
-                
+
             print("Current string:", text_str)
             last_class = class_name
 
