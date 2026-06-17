07f1531cb46e67b4b06e80bb9c0774c47e5e04c6
LucaMawyin <lucamawyin@gmail.com>
2026-06-17 02:11:38 -0400

    reconfigured yolo setup

--- yolo_model.py
+++ yolo_model.py
@@ -2,15 +2,15 @@ from ultralytics import YOLO
 
 def main():
 
-    model = YOLO("yolo26s.pt")
+    model = YOLO("yolo26m.pt")
 
     model.train(
         data="config.yaml",
-        epochs=300, 
-        patience=50,
+        epochs=200, 
+        patience=40,
         device=0, # Nvidia 3080ti
         workers=12, # i5 13600kf 14C 20T
-        batch=32,
+        batch=16,
         project=r"C:\Users\lucam\Desktop\Code\signlanguage\runs", # Explicitly state dir (saving to wrong dir otherwise) 
         name="hand_pose"
     )
